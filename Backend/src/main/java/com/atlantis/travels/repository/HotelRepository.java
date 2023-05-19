e66c5e9578619af25a1a0afe5f1615c39d7bb69c
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-05-19 17:43:36 +0100

    Init 2

--- Backend/src/main/java/com/atlantis/travels/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atlantis.travels.repository;
-
-import com.atlantis.travels.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
